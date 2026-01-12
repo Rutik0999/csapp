9989a2e5fb90a62af85f70f2afa5b534e1ff9ad9
Rutik Kohakade <rutikkohakade0999@gmail.com>
2026-01-12 21:29:57 +0530

    Initial commit

---
+++ src/main/java/com/csapp/cloudcode/CsappApplication.java
@@ -0,0 +1,13 @@
+package com.csapp.cloudcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/csapp/cloudcode/entities/Customer.java
@@ -0,0 +1,30 @@
+package com.csapp.cloudcode.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="customer")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customerId;
+
+
+    @Column(name = "customer_name")
+    private String customerName;
+    private String customerEmail;
+    private String password;
+    private String address;
+    @Column(name = "about_customer")
+
+    private String about;
+}
